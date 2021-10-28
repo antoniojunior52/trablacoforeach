0c58f5c9c630fb3574f642f360c487f4a3e16611
Antonio Junior <antoniocarlosjuninhojuca@gmail.com>
2021-10-28 18:25:36 -0300

    lacoforeach

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExGrupo
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] array = {"SP é São Paulo", "RJ é Rio de Janeiro", "PE é Recife", "AM é Manaus", "GO é Goiânia", "SC é Florianópolis"};
+
+            foreach(string local in array) 
+            {
+                Console.WriteLine($" A capital de {local}");
+            }
+        }
+    }
+}
